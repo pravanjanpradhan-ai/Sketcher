75da65092752145c3e051b0f8cdac8e25d3e9ad1
Rituraj Prasad <rituraj.prasad2718@gmail.com>
2025-09-24 11:21:48 +0530

    Chages on read File

--- SketcherGeometryEngine/src/FileWrite.cpp
+++ SketcherGeometryEngine/src/FileWrite.cpp
@@ -131,3 +131,93 @@ bool FileWrite::read(const std::string& filename, std::unordered_map<int, std::v
     fin.close();
     return true;
 }
+
+//bool FileWrite::read(const std::string& filename, std::unordered_map<int, std::vector<SketchData>>& shapes)
+//{
+//    std::ifstream fin(filename);
+//    if (!fin.is_open())
+//        return false;
+//
+//    shapes.clear();
+//    std::string line;
+//    int currentId = 0;
+//
+//    std::string currentShapeName;
+//    std::vector<Point> tempPoints;
+//    bool anyValidData = false;   // track if we parsed at least one valid shape/point
+//
+//    auto finalizeShape = [&](std::string shapeName, std::vector<Point>& pts) -> bool {
+//        Shape* newShape = nullptr;
+//        if (shapeName == "Line" && pts.size() >= 2) {
+//            newShape = new Line(pts[0], pts[1]);
+//        }
+//        else if (shapeName == "Rectangle" && pts.size() >= 4) {
+//            newShape = new Rectangles(pts[0], pts[2]);
+//        }
+//        else if (shapeName == "Circle" && pts.size() >= 2) {
+//            newShape = new Circle(pts[0], pts[1]);
+//        }
+//        else if (shapeName == "Triangle" && pts.size() >= 3) {
+//            newShape = new Triangle(pts[0], pts[1], pts[2]);
+//        }
+//
+//        if (newShape) {
+//            shapes[currentId++].push_back(newShape);
+//            pts.clear();
+//            return true;
+//        }
+//        return false;
+//        };
+//
+//    while (std::getline(fin, line)) {
+//        if (line.empty()) continue;
+//
+//        if (line.back() == ':') {
+//            // Finalize previous shape
+//            if (!currentShapeName.empty() && currentShapeName != "Point" && !tempPoints.empty()) {
+//                if (!finalizeShape(currentShapeName, tempPoints)) {
+//                    return false; // invalid shape format
+//                }
+//                anyValidData = true;
+//            }
+//
+//            // Start new shape
+//            currentShapeName = line.substr(0, line.size() - 1);
+//
+//            // Validate shape name
+//            if (currentShapeName != "Point" && currentShapeName != "Line" &&
+//                currentShapeName != "Triangle" && currentShapeName != "Rectangle" &&
+//                currentShapeName != "Circle") {
+//                return false; // unknown shape
+//            }
+//        }
+//        else {
+//            // Parse coordinates
+//            std::istringstream iss(line);
+//            double x, y;
+//            char comma;
+//            if (!(iss >> x >> comma >> y) || comma != ',') {
+//                return false;
+//            }
+//
+//            if (currentShapeName == "Point") {
+//                shapes[currentId++].push_back(Point(x, y));
+//                anyValidData = true;
+//            }
+//            else {
+//                tempPoints.push_back(Point(x, y));
+//            }
+//        }
+//    }
+//
+//    // Final shape at EOF
+//    if (!currentShapeName.empty() && currentShapeName != "Point" && !tempPoints.empty()) {
+//        if (!finalizeShape(currentShapeName, tempPoints)) {
+//            return false; // incomplete shape
+//        }
+//        anyValidData = true;
+//    }
+//
+//    fin.close();
+//    return anyValidData; // false if file empty or no valid shape/point
+//}
